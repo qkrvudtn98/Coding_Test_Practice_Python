ce15b783fd1b37e4bd4e2ff42531ca77cff13760
qkrvudtn98 <ps3624@naver.com>
2022-07-04 13:03:56 +0900

    백준 알고리즘_그리디: 신입 사원 (Silver1) 문제풀이 (정답)

--- ThisIsCodingTest/Greedy/BOJ1946_신입사원.py
+++ ThisIsCodingTest/Greedy/BOJ1946_신입사원.py
@@ -1,43 +1,23 @@
+import sys
 # 테스트 케이스 개수 입력
 t = int(input())
 
-# 테스트 케이스 개수만큼 신입사원 성적 입력
-score = []
-count_one = []
-count_two = []
-count = 0
-result = []
 for i in range(t):
+    score = []
+    count = 1
     # 테스트 케이스 입력
     n = int(input())
-    [score.append(list(map(int, input().split()))) for j in range(n)]
-    # 1차 시험 기준 정렬 / 2차 시험 기준 정렬
-    one = sorted(score, key=lambda x : x[0])
-    two = sorted(score, key=lambda x : x[1])
+    for j in range(n):
+        paper, interview = map(int, sys.stdin.readline().split())
+        score.append([paper, interview])
 
-    count_one.append(one[0])
-    count_two.append(two[0])
+    score.sort()
+    # 서류 1등의 면접 순위가 기준
+    max_score = score[0][1]
 
-    for j in range(1,n):
-        if one[j][1] < one[0][1]:
-            count_one.append(one[j])
-        if two[j][0] < two[0][0]:
-            count_two.append(two[j])
+    for j in range(1, n):
+        if max_score > score[j][1]:
+            count += 1
+            max_score = score[j][1]
 
-    for k in range(len(count_one)):
-        for j in range(len(count_two)):
-            if count_one[k] == count_two[j]:
-                count += 1
-            else:
-                continue
-
-    result.append(count)
-
-    # 테스트 케이스 종료 후 신입사원 성적 리스트 초기화
-    score = []
-    count_one = []
-    count_two = []
-    count = 0
-
-for i in range(t):
-    print(result[i])
+    print(count)
